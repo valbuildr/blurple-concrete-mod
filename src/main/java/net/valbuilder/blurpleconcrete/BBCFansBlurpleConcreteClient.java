234cb5f109fe5e343cb57e9c7232ab49e095e531
Valentine <v4lbuild3r@gmail.com>
2024-02-12 21:16:30 -0600

    Initial commit

---
+++ src/main/java/net/valbuilder/blurpleconcrete/BBCFansBlurpleConcreteClient.java
@@ -0,0 +1,10 @@
+package net.valbuilder.blurpleconcrete;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BBCFansBlurpleConcreteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
